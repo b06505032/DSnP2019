5baaf0bb0977d1898da81d82a487e46d84f81de0
b06505032 <b06505032@ntu.edu.tw>
2019-11-17 21:59:45 +0800

    11/17 22:00

--- hw5/src/util/bst.h
+++ hw5/src/util/bst.h
@@ -27,11 +27,7 @@ class BSTreeNode
    friend class BSTree<T>;
    friend class BSTree<T>::iterator;
 
-   // BSTreeNode(const T& d): 
-      // _data(d) {_left = _right = _parent = NULL; }
-   // BSTreeNode(const T& d, BSTreeNode<T>* p = NULL): 
-      // _data(d), _parent(p), _left(NULL), _right(NULL) {}
-   BSTreeNode(){}
+   // BSTreeNode(){}
    BSTreeNode(const T& d, BSTreeNode<T>* p = NULL, BSTreeNode<T>* l = NULL, BSTreeNode<T>* r = NULL):
       _data(d), _parent(p), _left(l), _right(r) {}
    
@@ -47,12 +43,13 @@ class BSTree
 {
    // TODO: design your own class!!
 public:
-   BSTree(){
-      _root = NULL;
+   BSTree() {
+      // _root = NULL;
+      _root = 0; _dummy = 0;
    }
    ~BSTree() {
       clear();
-      delete _root;
+      delete _dummy;
    }
    class iterator
    { 
@@ -71,7 +68,7 @@ public:
          return *(this);
       }
       iterator operator ++ (int) { iterator tmp = *(this); ++*(this); return tmp; }
-      iterator& operator -- () { return *(this); }
+      iterator& operator -- () { return *(this); } // not done yet
       iterator operator -- (int) { iterator tmp = *(this); --*(this); return tmp; }
 
       iterator& operator = (const iterator& i) { _node = i._node; return *(this); }
@@ -84,21 +81,60 @@ public:
       BSTreeNode<T>* _node;
    };
    
-   iterator begin() const { return iterator(leftmost(_root)); }
+   iterator begin() const { 
+      if (size()==0) 
+         return end();
+      else 
+         return iterator(leftmost(_root)); 
+   }
    iterator end() const { return iterator(_dummy); }
    bool empty() const { return size() == 0;}
    size_t size() const { return _size; }
    void insert(const T& x) { 
-      if (_root == NULL) {
-         _root = new BSTreeNode<T>(x,NULL, NULL, NULL);
+      if (size()==0) { // set the root first
+         cout<<"build up the first node!"<<endl;
+         BSTreeNode<T>* newNode = new BSTreeNode<T>(x);
+         _root = newNode;
+         _dummy = new BSTreeNode<T>(x);
+         _dummy->_left = _root;
+         _root->_right = _dummy;
+         // _root->_parent = _dummy;
       } else {
          insertHelper(x, _root);
       }
-      _size++;
-      // InorderPrint (); // check if it is Inorder
+      _size+=1;
+      // InorderPrint ();
    }
-   void pop_front(){ } // leftmost node (i.e. begin()) will be removed.
-   void pop_back() { } // remove the last (rightmost for BSTree)
+   void pop_front(){ // leftmost node (i.e. begin()) will be removed.
+      if(empty()) return;
+      BSTreeNode<T>* temp = leftmost(_root);
+      if(size()==1) {
+         cout<< "delete the only node!"<<endl;
+         _root = 0;
+         _dummy = 0;
+         _size = 0;
+         return;
+      }
+      if(temp == _root) {
+         cout<< "left most is the root!"<<endl;
+         _root = temp->_right;
+      }
+      else {
+         cout<<"else!"<<endl;
+         if(temp->_right == NULL) {
+            temp->_parent->_left = NULL;
+         }
+         else {
+            temp->_parent->_left = temp->_right;
+         }
+      }
+      delete temp;
+      _size--;
+      return;
+   } 
+   void pop_back() { // remove the last (rightmost for BSTree)
+      
+   } 
    bool erase(const T& x) { return true;}
    bool erase(iterator pos) { return true;}
    iterator find(const T& x) { return end();}
@@ -109,25 +145,33 @@ public:
 private:
    BSTreeNode<T>* _root;
    BSTreeNode<T>* _dummy;
-   int _size;
-   mutable bool _isSorted;
+   int _size = 0;
 
    void insertHelper(const T data, BSTreeNode<T>* rootnode) {
-      if (data<=(rootnode->_data)) {
-         if (rootnode->_left == NULL) {
-            rootnode->_left = new BSTreeNode<T>(data, rootnode, NULL, NULL);
-         } else {
+      BSTreeNode<T>* t = new BSTreeNode<T>(data, rootnode, NULL, NULL);
+      if (data<=(rootnode->_data)) { 
+         if (rootnode->_left == NULL) {  
+            rootnode->_left = t;
+         }
+         else 
             insertHelper(data, rootnode->_left);
+      } 
+      else { 
+         if (rootnode->_right == _dummy) {
+            rootnode->_right = t;
+            t->_parent = rootnode;
+            t->_right = _dummy;
+            _dummy->_left = t;
          }
-      } else {
-         if (rootnode->_right == NULL) {
-            rootnode->_right = new BSTreeNode<T>(data, rootnode, NULL, NULL);
-         } else {
+         else if (rootnode->_right == NULL) { 
+            rootnode->_right = t;
+         }
+         else
             insertHelper(data, rootnode->_right);
-         }
       }
    }
 
+   // The function to find the leftmost node
    static BSTreeNode<T>* leftmost(BSTreeNode<T> *current){
       while (current->_left != NULL) {
         current = current->_left;
@@ -135,9 +179,23 @@ private:
       return current;
    }
 
-   static BSTreeNode<T>* successor(BSTreeNode<T> *current){
+   static BSTreeNode<T>* rightmost(BSTreeNode<T> *current){
+      while (current->_right != NULL) {
+        current = current->_right;
+      }
+      return current;
+   }
+
+   // The function to find the minimum node of right descendant
+   static BSTreeNode<T>* successor(BSTreeNode<T> *current) {
       if (current->_right != NULL) {
-         return leftmost(current->_right);
+         if(current->_right->_left != current) {
+            current =  leftmost(current->_right);
+            return current;
+         } else {
+           current = current->_right;
+           return current;
+         }
       }
       BSTreeNode<T>* successor = current->_parent;   
       while (successor != NULL && current == successor->_right) {
